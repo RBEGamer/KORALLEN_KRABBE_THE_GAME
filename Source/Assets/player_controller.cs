37442ab4441670b81cde4cbd7b851eee4b448b48
RBEGamer <marcel.ochsendorf@gmail.com>
2017-06-10 21:11:07 +0200

    added player move lerp

--- Source/Assets/player_controller.cs
+++ Source/Assets/player_controller.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class player_controller : MonoBehaviour {
 
+
+
+	public Vector3 should_have_position;
+	public float movement_speed = 1.0f;
 	// Use this for initialization
 	void Start () {
 		this.name = "player";
+		should_have_position = this.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+
+
+		this.transform.position = Vector3.Lerp(this.transform.position, should_have_position, movement_speed);
 	}
 		
 
