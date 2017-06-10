0c8014dc0b863ca5a229f95a33ba9ae4c6376a9f
RBEGamer <marcel.ochsendorf@gmail.com>
2017-06-10 22:47:30 +0200

    fix ?

--- Source/Assets/api/garbage/GarbageItem.cs
+++ Source/Assets/api/garbage/GarbageItem.cs
@@ -12,7 +12,7 @@ public class GarbageItem : MonoBehaviour
 
     private IEnumerator Start()
     {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(2.0f);
         IsAlive = true;
     }
 
