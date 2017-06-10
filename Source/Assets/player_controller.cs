4d34c7ec915cae2ffa180aee67232ba16020bd1f
RBEGamer <marcel.ochsendorf@gmail.com>
2017-06-10 21:06:33 +0200

    food clear on player coll

---
+++ Source/Assets/player_controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.name = "player";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+
+}
